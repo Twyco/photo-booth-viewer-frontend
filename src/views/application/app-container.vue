eafa54db285bd5106d082b349b646d65e8b4ba7f
Twyco <twyco@icloud.com>
2024-11-27 05:16:30 +0100

    fixed Scrollbar

--- src/views/application/app-container.vue
+++ src/views/application/app-container.vue
@@ -4,16 +4,14 @@ import MenuBar from "@views/hud/menu/menu-bar.vue";
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app>
-      <menu-bar/>
-      <v-main>
-        <v-container>
-          <slot/>
-        </v-container>
-      </v-main>
-    </v-app>
-  </v-responsive>
+  <v-app>
+    <menu-bar/>
+    <v-main>
+      <v-container>
+        <slot/>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
